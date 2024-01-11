bc1bfd04726388736cb55dc6723cc0230864391a
ecorralro <ecorralr689@alumnos.imf.com>
2024-01-11 15:09:11 +0100

    flujos entrada y salida
    flujos de entrada y salida tanto en cliente como en servidor.

--- src/cliente_servidor/Servidor.java
+++ src/cliente_servidor/Servidor.java
@@ -1,6 +1,9 @@
 package cliente_servidor;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -17,6 +20,10 @@ public class Servidor {
 			// acepto la conexión del socket del cliente
 			Socket clienteSocket = servidorSocket.accept();
 			System.out.println("La conexión se ha establecido con el cliente.");
+			//flujos de entrada y salida para comunicarse con el cliente
+			BufferedReader entradaCliente = new BufferedReader(new InputStreamReader(clienteSocket.getInputStream()));
+			PrintWriter salidaCliente = new PrintWriter(clienteSocket.getOutputStream(), true);// true realiza autoflush sin necesidad de hacer un posterior salidaCliente.flush()
+			
 			
 		} catch (IOException e) {
 			e.printStackTrace();
