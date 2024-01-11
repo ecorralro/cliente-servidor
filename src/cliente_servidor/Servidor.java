fae004b214716b32fa87473346ca06fce2a53e4f
ecorralro <ecorralr689@alumnos.imf.com>
2024-01-11 14:37:02 +0100

    sockets
    creo los sockets del cliente y del servidor y los conecto con el host y el puerto

--- src/cliente_servidor/Servidor.java
+++ src/cliente_servidor/Servidor.java
@@ -1,9 +1,28 @@
 package cliente_servidor;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Servidor {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Declaración de variables
+		final int puerto = 5678;
+		
+		try {
+			// creo socket y espero conexión con el puerto
+			ServerSocket servidorSocket = new ServerSocket(puerto);
+			System.out.println("Servidor escuchando en el puerto: " + puerto);
+			// acepto la conexión del socket del cliente
+			Socket clienteSocket = servidorSocket.accept();
+			System.out.println("La conexión se ha establecido con el cliente.");
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			// cerrar conexion
+		}
 
 	}
 
